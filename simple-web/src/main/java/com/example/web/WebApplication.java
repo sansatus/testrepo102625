defc21e69cc978f2195cac5cdb05a551d289db59
vinodmadarapu <vinod.madarapu@gmail.com>
2025-10-26 12:14:09 -0500

    Import workspace code

---
+++ simple-web/src/main/java/com/example/web/WebApplication.java
@@ -0,0 +1,20 @@
+package com.example.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@Controller
+public class WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
